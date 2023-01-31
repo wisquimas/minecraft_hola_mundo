fb35248198d09ec63dca26abb5bffe687105fab8
Wisquimas <wisquimas@gmail.com>
2023-01-31 16:04:35 -0600

    inicio

---
+++ src/main/java/com/rehilete_digital/hola_mundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package com.rehilete_digital.hola_mundo;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(HolaMundo.MODID)
+public class HolaMundo {
+    public static final String MODID = "hola_mundo";
+
+}
